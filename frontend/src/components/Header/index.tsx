67c05ce6d9911c564f7bfbb766d3f24e1781843f
RonaldoCesar28 <ronaldocesar28@outlook.com>
2022-07-26 15:43:14 -0300

    Atualizacao do Header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ function Header() {
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
-                <h1>RCTecnologia</h1>  
+                <h1>DSMeta</h1>  
                 <p>
                     Desenvolvido por
                     <a href="https://instagram.com/ronaldo_developer"> @RonaldoCesar</a>
